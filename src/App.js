548256e5698af1fa02206e3b56375f0304a27c89
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-01 11:03:48 +1000

    fetching weather data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import WeekComponent from "./component/WeekComponent";
+// Api_key = "ce844b724a6d3ebad7e0a8653133cf69";
+// const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${Api_key}`;
 
 function App() {
+  useEffect(() => {
+    //   effect;
+    getWeather();
+    //   return () => {
+    //     cleanup;
+    //   };
+  }, []);
+
+  const getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=london&units=imperial&APPID=${"ce844b724a6d3ebad7e0a8653133cf69"}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WeekComponent />
     </div>
   );
 }
---
+++ src/component/WeekComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WeekComponent() {
+    return (
+        <div>
+            iam WeekComponent
+        </div>
+    )
+}
+
+export default WeekComponent
