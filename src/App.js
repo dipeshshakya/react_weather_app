47dc9913cb57b9fcb6f6a2cdcd930745a5bcaa5c
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-02 15:27:35 +1000

    search functionality

--- src/App.js
+++ src/App.js
@@ -2,38 +2,69 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import WeatherWrapperComponent from "./component/WeatherWrapperComponent";
 import DayCard from "./DayCard";
-// Api_key = "ce844b724a6d3ebad7e0a8653133cf69";
-// const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${Api_key}`;
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
 
 function App() {
   const [fullData, setfullData] = useState([]);
   const [DailyData, setDailyData] = useState([]);
+  const [query, setQuery] = useState("australia");
+  const [formValues, setFormValues] = useState({});
 
-  const [Loading, setLoading] = useState(true);
+  // const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
     //   effect;
     const getWeather = async () => {
       await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?q=australia&units=imperial&APPID=${"ce844b724a6d3ebad7e0a8653133cf69"}`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${query}&units=imperial&APPID=${"ce844b724a6d3ebad7e0a8653133cf69"}`
       )
         .then((response) => response.json())
         .then((data) => {
           const dailyData = data.list.filter((reading) =>
             reading.dt_txt.includes("18:00:00")
           );
-          setfullData(data.list);
+          setfullData(data.city);
           setDailyData(dailyData);
         });
     };
     getWeather();
-    setLoading(false);
-  }, []);
-  console.log(DailyData);
+    // setLoading(false);
+  }, [query]);
+  console.log(fullData);
+  const handleChange = (event) => {
+    // let name = event.target.name;
+    let value = event.target.value;
+
+    setFormValues({
+      name: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(formValues.name);
+  };
 
   return (
     <div className="App">
-      <WeatherWrapperComponent />
+      <div className="form__wrapper">
+        <Paper elevation={3}>
+          <form action="" onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              name="city"
+              onChange={handleChange}
+              style={{
+                margin: "0 auto",
+                maxWidth: 800,
+              }}
+            />
+            <button type="submit">Go</button>
+          </form>
+        </Paper>
+      </div>
+
+      <WeatherWrapperComponent fullData={fullData} />
       <div className="dayCard-outerWrapper">
         {DailyData.map((reading, index) => (
           <DayCard reading={reading} key={index} />
--- src/component/WeatherWrapperComponent.js
+++ src/component/WeatherWrapperComponent.js
@@ -1,9 +1,29 @@
 import React from "react";
+var moment = require("moment");
+
 // import Brightness1RoundedIcon from "@material-ui/icons/Brightness1Rounded";
 // import Brightness3RoundedIcon from "@material-ui/icons/Brightness3Rounded";
 // import { yellow } from "@material-ui/core/colors";
-function WeatherWrapperComponent() {
-  return <div className="weatherWrapper"></div>;
+function WeatherWrapperComponent({ fullData }) {
+  const sunRise = fullData.sunrise * 1000;
+  const sunSet = fullData.sunset * 1000;
+
+  console.log(fullData);
+  return (
+    <div className="weatherWrapper">
+      <h2 className="city_txt">{fullData.name}</h2>
+      <div className="sunTime">
+        <div className="sunrise">
+          {moment(new Date().setTime(sunRise)).format("h:mm a")}
+          <p className="sunTime__text_color">sunrise</p>
+        </div>
+        <div className="sunset">
+          {moment(new Date().setTime(sunSet)).format("h:mm a")}
+          <p className="sunTime__text_color">sunset</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WeatherWrapperComponent;
